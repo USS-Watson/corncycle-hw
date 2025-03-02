ed2fe4097d5072301551dff31ef5991944f0b07e
Blaine Traudt <blaine-t@bathost.net>
2025-03-01 21:37:16 -0600

    Testing pins on real car
    What is happening send help

--- full_throttle/full_throttle.ino
+++ full_throttle/full_throttle.ino
@@ -1,28 +1,67 @@
-const int FRONT_LEFT_FWD = 15;
-const int FRONT_LEFT_REV = 16;
-const int FRONT_RIGHT_FWD = 17;
-const int FRONT_RIGHT_REV = 18;
-const int BACK_LEFT_FWD = 19;
-const int BACK_LEFT_REV = 21;
-const int BACK_RIGHT_FWD = 22;
-const int BACK_RIGHT_REV = 23;
+const int FRONT_LEFT_FWD = 3;
+const int FRONT_LEFT_REV = 2;
+const int FRONT_RIGHT_FWD = 11;
+const int FRONT_RIGHT_REV = 10;
+const int BACK_LEFT_FWD = 8;
+const int BACK_LEFT_REV = 1;
+const int BACK_RIGHT_FWD = 0;
+const int BACK_RIGHT_REV = 7;
+// const int BACK_RIGHT_FWD = 18;
+// const int BACK_RIGHT_REV = 19;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  analogWrite(FRONT_LEFT_FWD, 255);
-  analogWrite(FRONT_RIGHT_FWD, 255);
-  analogWrite(BACK_LEFT_FWD, 255);
-  analogWrite(BACK_RIGHT_FWD, 255);
-  analogWrite(FRONT_LEFT_REV, 0);
-  analogWrite(FRONT_RIGHT_REV, 0);
-  analogWrite(BACK_LEFT_REV, 0);
-  analogWrite(BACK_RIGHT_REV, 0);
+  pinMode(BACK_LEFT_FWD, OUTPUT);
+  pinMode(BACK_LEFT_REV, OUTPUT);
+  pinMode(BACK_RIGHT_FWD, OUTPUT);
+  pinMode(BACK_RIGHT_REV, OUTPUT);
+  pinMode(FRONT_LEFT_FWD, OUTPUT);
+  pinMode(FRONT_LEFT_REV, OUTPUT);
+  pinMode(FRONT_RIGHT_FWD, OUTPUT);
+  pinMode(FRONT_RIGHT_REV, OUTPUT);
+  digitalWrite(BACK_LEFT_FWD, LOW);
+  digitalWrite(BACK_LEFT_REV, LOW);
+  digitalWrite(BACK_RIGHT_FWD, LOW);
+  digitalWrite(BACK_RIGHT_REV, LOW);
+  digitalWrite(FRONT_LEFT_FWD, LOW);
+  digitalWrite(FRONT_LEFT_REV, LOW);
+  digitalWrite(FRONT_RIGHT_FWD, LOW);
+  digitalWrite(FRONT_RIGHT_REV, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
-    Serial.println(Serial.readStringUntil('\n'));
-  }
+  digitalWrite(FRONT_LEFT_FWD, HIGH);
+  Serial.println("FRONT_LEFT_FWD");
+  delay(1500);
+  digitalWrite(FRONT_LEFT_FWD, LOW);
+  digitalWrite(FRONT_LEFT_REV, HIGH);
+  Serial.println("FRONT_LEFT_REV");
+  delay(1500);
+  digitalWrite(FRONT_LEFT_REV, LOW);
+  digitalWrite(FRONT_RIGHT_FWD, HIGH);
+  Serial.println("FRONT_RIGHT_FWD");
+  delay(1500);
+  digitalWrite(FRONT_RIGHT_FWD, LOW);
+  digitalWrite(FRONT_RIGHT_REV, HIGH);
+  Serial.println("FRONT_RIGHT_REV");
+  delay(1500);
+  digitalWrite(FRONT_RIGHT_REV, LOW);
+  digitalWrite(BACK_LEFT_FWD, HIGH);
+  Serial.println("BACK_LEFT_FWD");
+  delay(1500);
+  digitalWrite(BACK_LEFT_FWD, LOW);
+  digitalWrite(BACK_LEFT_REV, HIGH);
+  Serial.println("BACK_LEFT_REV");
+  delay(1500);
+  digitalWrite(BACK_LEFT_REV, LOW);
+  digitalWrite(BACK_RIGHT_FWD, HIGH);
+  Serial.println("BACK_RIGHT_FWD");
+  delay(1500);
+  digitalWrite(BACK_RIGHT_FWD, LOW);
+  digitalWrite(BACK_RIGHT_REV, HIGH);
+  Serial.println("BACK_RIGHT_REV");
+  delay(1500);
+  digitalWrite(BACK_RIGHT_REV, LOW);
 }
