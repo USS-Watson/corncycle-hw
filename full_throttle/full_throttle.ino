4e6908ba1f9378dd10e223a213f53c6320950bfe
Blaine Traudt <blaine-t@bathost.net>
2025-03-01 23:28:06 -0600

    RIP quad h bridge

--- full_throttle/full_throttle.ino
+++ full_throttle/full_throttle.ino
@@ -1,67 +1,36 @@
-const int FRONT_LEFT_FWD = 3;
-const int FRONT_LEFT_REV = 2;
-const int FRONT_RIGHT_FWD = 11;
-const int FRONT_RIGHT_REV = 10;
-const int BACK_LEFT_FWD = 8;
-const int BACK_LEFT_REV = 1;
-const int BACK_RIGHT_FWD = 0;
-const int BACK_RIGHT_REV = 7;
-// const int BACK_RIGHT_FWD = 18;
-// const int BACK_RIGHT_REV = 19;
+const int LEFT_FWD = 3;
+const int LEFT_REV = 2;
+const int RIGHT_FWD = 11;
+const int RIGHT_REV = 10;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(BACK_LEFT_FWD, OUTPUT);
-  pinMode(BACK_LEFT_REV, OUTPUT);
-  pinMode(BACK_RIGHT_FWD, OUTPUT);
-  pinMode(BACK_RIGHT_REV, OUTPUT);
-  pinMode(FRONT_LEFT_FWD, OUTPUT);
-  pinMode(FRONT_LEFT_REV, OUTPUT);
-  pinMode(FRONT_RIGHT_FWD, OUTPUT);
-  pinMode(FRONT_RIGHT_REV, OUTPUT);
-  digitalWrite(BACK_LEFT_FWD, LOW);
-  digitalWrite(BACK_LEFT_REV, LOW);
-  digitalWrite(BACK_RIGHT_FWD, LOW);
-  digitalWrite(BACK_RIGHT_REV, LOW);
-  digitalWrite(FRONT_LEFT_FWD, LOW);
-  digitalWrite(FRONT_LEFT_REV, LOW);
-  digitalWrite(FRONT_RIGHT_FWD, LOW);
-  digitalWrite(FRONT_RIGHT_REV, LOW);
+  pinMode(LEFT_FWD, OUTPUT);
+  pinMode(LEFT_REV, OUTPUT);
+  pinMode(RIGHT_FWD, OUTPUT);
+  pinMode(RIGHT_REV, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(FRONT_LEFT_FWD, HIGH);
-  Serial.println("FRONT_LEFT_FWD");
-  delay(1500);
-  digitalWrite(FRONT_LEFT_FWD, LOW);
-  digitalWrite(FRONT_LEFT_REV, HIGH);
-  Serial.println("FRONT_LEFT_REV");
-  delay(1500);
-  digitalWrite(FRONT_LEFT_REV, LOW);
-  digitalWrite(FRONT_RIGHT_FWD, HIGH);
-  Serial.println("FRONT_RIGHT_FWD");
-  delay(1500);
-  digitalWrite(FRONT_RIGHT_FWD, LOW);
-  digitalWrite(FRONT_RIGHT_REV, HIGH);
-  Serial.println("FRONT_RIGHT_REV");
-  delay(1500);
-  digitalWrite(FRONT_RIGHT_REV, LOW);
-  digitalWrite(BACK_LEFT_FWD, HIGH);
-  Serial.println("BACK_LEFT_FWD");
-  delay(1500);
-  digitalWrite(BACK_LEFT_FWD, LOW);
-  digitalWrite(BACK_LEFT_REV, HIGH);
-  Serial.println("BACK_LEFT_REV");
-  delay(1500);
-  digitalWrite(BACK_LEFT_REV, LOW);
-  digitalWrite(BACK_RIGHT_FWD, HIGH);
-  Serial.println("BACK_RIGHT_FWD");
-  delay(1500);
-  digitalWrite(BACK_RIGHT_FWD, LOW);
-  digitalWrite(BACK_RIGHT_REV, HIGH);
-  Serial.println("BACK_RIGHT_REV");
-  delay(1500);
-  digitalWrite(BACK_RIGHT_REV, LOW);
+  analogWrite(LEFT_FWD, 127);
+  analogWrite(LEFT_REV, 0);
+  analogWrite(RIGHT_FWD, 127);
+  analogWrite(RIGHT_REV, 0);
+  delay(2500);
+  analogWrite(LEFT_FWD, 127);
+  analogWrite(LEFT_REV, 0);
+  analogWrite(RIGHT_FWD, 0);
+  analogWrite(RIGHT_REV, 127);
+  delay(2500);
+  analogWrite(LEFT_FWD, 0);
+  analogWrite(LEFT_REV, 127);
+  analogWrite(RIGHT_FWD, 0);
+  analogWrite(RIGHT_REV, 127);
+  delay(2500);
+  analogWrite(LEFT_FWD, 0);
+  analogWrite(LEFT_REV, 127);
+  analogWrite(RIGHT_FWD, 127);
+  analogWrite(RIGHT_REV, 0);
+  delay(2500);
 }
